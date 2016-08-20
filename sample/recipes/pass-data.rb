96ab7db5e62e1bc4224e7757415647ca4c88eba7
Steven YU <steven.yu@polarseven.com>
2016-08-21 05:57:50 +1000

    test search

--- sample/recipes/pass-data.rb
+++ sample/recipes/pass-data.rb
@@ -1,5 +1,7 @@
-node[:deploy].each do |app, deploy|
-  file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
-    content YAML.dump(node[:my_app_data][app].to_hash)
-  end
-end
+search(:aws_opsworks_stack)
+
+# node[:deploy].each do |app, deploy|
+#   file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
+#     content YAML.dump(node[:my_app_data][app].to_hash)
+#   end
+# end
