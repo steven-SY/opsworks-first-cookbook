c521c38ef59387c964e9b7eb47a4c1627e90c6db
Steven YU <steven.yu@polarseven.com>
2016-08-21 06:06:44 +1000

    databag command

--- sample/recipes/pass-data.rb
+++ sample/recipes/pass-data.rb
@@ -1,4 +1,25 @@
-search(:aws_opsworks_stack)
+stack = search("aws_opsworks_stack").first
+Chef::Log.info("********** The stack's name is '#{stack['name']}' **********")
+Chef::Log.info("********** The stack's cookbook URL is '#{stack['custom_cookbooks_source']['url']}' **********")
+
+instance = search("aws_opsworks_instance", "self:true").first
+Chef::Log.info("********** For instance '#{instance['instance_id']}', the instance's operating system is '#{instance['os']}' **********")
+
+search("aws_opsworks_app").each do |app|
+  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+end
+
+search("aws_opsworks_command").each do |command|
+  Chef::Log.info("********** The command's type is '#{command['type']}' **********")
+  Chef::Log.info("********** The command was sent at '#{command['sent_at']}' **********")
+end
+
+data_bag("aws_opsworks_app").each do |data_bag_item|
+  app = data_bag_item("aws_opsworks_app", data_bag_item)
+  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+end
 
 # node[:deploy].each do |app, deploy|
 #   file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
